470d5551299fc287411c62d7f4906f4f985ad90d
certihimo <kalidahm3d@gmail.com>
2022-12-13 12:15:09 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.getElementById("create-task-form").addEventListener("submit", (e)=>{
-    e.preventDefault()
-    submitBtn(e.target.input.value)
-  })
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleTodo();
+    form.reset();
+  });
 
-})
-  function submitBtn(task){
-    let ul = document.getElementById('tasks')
-    let li = document.createElement('li')
-    ul.appendChild(li)
-    li.innerText = `${task} `
-    let x = document.createElement('button')
-    x.addEventListener("click", deleteElem)
-    x.innerText= 'X'
-    li.appendChild(x)
+  function handleTodo(todo) {
+    todo = document.getElementById("new-task-description").value;
+    let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", deleteToDo);
+    btn.textContent = "X"
+    li.textContent = `${todo} `;
+    let tasks = document.getElementById("tasks");
+    tasks.appendChild(li);
+    li.appendChild(btn);
   }
 
-  function deleteElem(e){
-    e.target.parentNode.remove()
+  function deleteToDo(e){
+e.target.parentNode.remove();
   }
-;
+});
